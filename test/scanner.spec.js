a620b077a97e20469857f817081ef4ff2c51de0c
Camilo Tapia <camilo.tapia@24hr.se>
2014-04-19 23:54:18 +0200

    check net id when recieving a message

--- test/scanner.spec.js
+++ test/scanner.spec.js
@@ -11,8 +11,8 @@ describe("The scanner part", function() {
 
         var counter = 0;
         
-        var node1 = zonar.create({name: "foo"}); 
-        var node2 = zonar.create({name: "bar"}); 
+        var node1 = zonar.create({net: "test", name: "foo"}); 
+        var node2 = zonar.create({net: "test", name: "bar"}); 
 
         node1.on('found', function(foundNode) {
             foundNode.should.have.property('name', 'bar');
@@ -35,17 +35,47 @@ describe("The scanner part", function() {
 
     });
 
+
+    it("will trigger an event with the service name when found", function(done) {
+
+        var counter = 0;
+        
+        var node1 = zonar.create({net: "test", name: "foo"}); 
+        var node2 = zonar.create({net: "test", name: "bar"}); 
+
+        node1.on('found.bar', function(foundNode) {
+            foundNode.should.have.property('name', 'bar');
+            if (++counter == 2) runTests(); 
+        });
+
+        node2.on('found.foo', function(foundNode) {
+            foundNode.should.have.property('name', 'foo');
+            if (++counter == 2) runTests(); 
+        });
+
+        function runTests() {
+            node1.stop(function() {
+                node2.stop(done);
+            });
+        }
+
+        node1.start();
+        node2.start();
+
+    });
+
+
+
+
     it("will create a list of all nodes in the net", function(done) {
 
         var counter = 0;
         
-        var node1 = zonar.create({name: "foo"}); 
-        var node2 = zonar.create({name: "bar"}); 
-        var node3 = zonar.create({name: "baq"}); 
+        var node1 = zonar.create({net: "test", name: "foo"}); 
+        var node2 = zonar.create({net: "test", name: "bar"}); 
+        var node3 = zonar.create({net: "test", name: "baq"}); 
 
         node1.on('found', function(foundNode) { if (++counter == 2) runTests(foundNode); });
-        //node2.on('found', function(foundNode) { if (++counter == 3) runTests(foundNode); });
-        //node3.on('found', function(foundNode) { if (++counter == 3) runTests(foundNode); });
 
         function runTests(foundNode) {
 
