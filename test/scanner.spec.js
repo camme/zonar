89ff6b2398837f629ff57ee8444a7940fbd80d5f
Camilo Tapia <camilo.tapia@24hr.se>
2014-04-20 18:39:00 +0200

    added check for same name nodes and added listening mode

--- test/scanner.spec.js
+++ test/scanner.spec.js
@@ -10,7 +10,7 @@ describe("The scanner part", function() {
     it("will find another node and emit an event", function(done) {
 
         var counter = 0;
-        
+
         var node1 = zonar.create({net: "test", name: "foo"}); 
         var node2 = zonar.create({net: "test", name: "bar"}); 
 
@@ -39,7 +39,7 @@ describe("The scanner part", function() {
     it("will trigger an event with the service name when found", function(done) {
 
         var counter = 0;
-        
+
         var node1 = zonar.create({net: "test", name: "foo"}); 
         var node2 = zonar.create({net: "test", name: "bar"}); 
 
@@ -64,13 +64,10 @@ describe("The scanner part", function() {
 
     });
 
-
-
-
     it("will create a list of all nodes in the net", function(done) {
 
         var counter = 0;
-        
+
         var node1 = zonar.create({net: "test", name: "foo"}); 
         var node2 = zonar.create({net: "test", name: "bar"}); 
         var node3 = zonar.create({net: "test", name: "baq"}); 
@@ -97,6 +94,72 @@ describe("The scanner part", function() {
         node3.start();
 
     });
+
+    it("will throw an error if a new service with a used name is started", function(done) {
+
+        var counter = 0;
+
+        var node1 = zonar.create({net: "test", name: "foo"}); 
+        var node2 = zonar.create({net: "test", name: "foo"}); 
+
+        var error = null;
+        node2.on('error', function(err) {
+            error = err;
+        });
+
+        node1.start();
+
+        // if we start them at exactly the same time, we get a strange reaction where
+        // both the first and second get the TAKEN error
+        setTimeout(function() {
+
+            node2.start();
+
+            setTimeout(function runTest() {
+                node1.stop(function() {
+                    node2.stop(function() {
+                        should.exist(error);
+                        done();
+                    });
+                });
+            }, 300);
+
+        }, 10);
+
+    });
+
+    it("will not update the node list if in listen mode", function(done) {
+
+        var counter = 0;
+
+        var node1 = zonar.create({net: "test", name: "foo"}); 
+        var node2 = zonar.create({net: "test", name: "bar"}); 
+        var node3 = zonar.create({net: "test", name: "baq"}); 
+
+        var c1 = c2 = c3 = 0;
+        node1.on('found', function(foundNode) { c1++; });
+        node2.on('found', function(foundNode) { c2++; });
+        node3.on('found', function(foundNode) { c3++; });
+
+        setTimeout(function runTests() {
+            c1.should.be.equal(1);
+            c2.should.be.equal(1);
+            c3.should.be.equal(2);
+
+            node1.stop(function() {
+                node2.stop(function() {
+                    node3.stop(done);
+                });
+            });
+
+        }, 300);
+
+        node1.start();
+        node2.start();
+        node3.listen();
+
+    });
+
 
 });
 
